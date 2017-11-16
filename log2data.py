c8ff42458ec276ddc109a107088701ef65cf44a9
kyll <kyllianbroers@live.nl>
2017-11-16 11:39:11 +0100

    can specify output file for log2data

--- log2data.py
+++ log2data.py
@@ -2,12 +2,13 @@ import sys
 from pytocl.analysis import DataLogReader
 import numpy as np
 
-if __name__ == "__main__" and len(sys.argv) == 2:
+if __name__ == "__main__" and len(sys.argv) >= 2:
     state_attributes = ['speed_x', 'speed_y', 'speed_z', 'distance_from_center', 'angle', 'distances_from_edge']
     command_attributes = ['accelerator', 'brake', 'steering']
     reader = DataLogReader(sys.argv[1], state_attributes, command_attributes)
     
-    with open('data.csv', 'w') as fout:
+    filename_out = sys.argv[2] if len(sys.argv) > 2 else 'data.csv'
+    with open(filename_out, 'w') as fout:
         header = 'ACCELERATION,BRAKE,STEERING,SPEED,TRACK_POSITION,ANGLE_TO_TRACK_AXIS,'
         header += ','.join(['TRACK_EDGE_' + str(i) for i in range(18)])
         fout.write(header + '\n')
