6a136a41f3dd1bbf28d8fb44e07ee77e30049bf3
kyll <kyllianbroers@live.nl>
2017-11-16 11:28:32 +0100

    finished log2data

--- log2data.py
+++ log2data.py
@@ -1,21 +1,20 @@
-import pickle 
-import sys, traceback
+import sys
+from pytocl.analysis import DataLogReader
 import numpy as np
 
 if __name__ == "__main__" and len(sys.argv) == 2:
-    with open(sys.argv[1], 'rb') as fin, open('data.csv', 'w') as fout:
-        while True:
-            try:                
-                state, command = pickle.load(fin)
-                speed = np.linalg.norm([state.speed_x, state.speed_y, state.speed_z])
-                line = str(command.accelerator)
-                line += ',' + str(command.brake)
-                line += ',' + str(command.steering)
-                line += ',' + str(speed)
-                line += ',' + str(state.distance_from_center)
-                line += ',' + str(state.angle)
-                line += ','.join([str(x) for x in state.distances_from_edge])
-                fout.write(line + '\n')
-            except EOFError:
-                break
+    state_attributes = ['speed_x', 'speed_y', 'speed_z', 'distance_from_center', 'angle', 'distances_from_edge']
+    command_attributes = ['accelerator', 'brake', 'steering']
+    reader = DataLogReader(sys.argv[1], state_attributes, command_attributes)
+    
+    with open('data.csv', 'w') as fout:
+        header = 'ACCELERATION,BRAKE,STEERING,SPEED,TRACK_POSITION,ANGLE_TO_TRACK_AXIS,'
+        header += ','.join(['TRACK_EDGE_' + str(i) for i in range(18)])
+        fout.write(header + '\n')
+        
+        for log in reader.array:
+            line = ','.join([str(x) for x in log[-3:]])
+            line += ',' + str(np.linalg.norm([log[1:4]]))
+            line += ',' + ','.join([str(x) for x in log[4:-3]])
+            fout.write(line + '\n')
         
